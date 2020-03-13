78a7edae0e42829c8ea706be43421bc32c23d5b3
Azdeman <cyber_azdeman@mail.ru>
2020-03-13 18:47:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,75 +1,16 @@
 var app = require('express')();
 var http = require('http').createServer(app);
-var io = require('socket.io')(http);
 var mysql = require('mysql');
-var request= require('request');
+var WebSocket = require('ws');
 
 app.get('/', (req, res)=>{
 	res.sendFile(__dirname + '/client.html');
 });
 
-var connection = mysql.createConnection({
-    host     : '31.31.198.115',
-    user     : 'u0942383_plannot',
-    password : '0N7j6Y9y',
-    database : 'u0942383_plannote'
-});
+const ws = new WebSocket.Server({port:3000});
 
-connection.connect();
-
-connection.query('SET SESSION wait_timeout=900000'); 
-connection.query('SET CHARACTER SET utf8'); 
-
-io.on('connection', function(socket){
-  console.log('a user connected');
-		socket.on('add_group',(data)=>{
-				socket.join(data.keygen); //добавляем пользователя в группу
-					//отправляем всем пользователям что подключился новый пользователь
-					socket.broadcast.to(data.keygen).emit('add_sinhronizate_platform','Подключился '+data.plathorm); 
-				//отправляем данные подключившемуся только что пользователю (заметки, шаблоны, ежедневные заметки)	
-			connection.query("SELECT `id` FROM `keygen` WHERE `key_`='"+data.keygen+"'",
-					(error,result,fields)=>{
-						if(result.length){
-							var object_info = [];
-								var id_keygen = JSON.parse(JSON.stringify(result))[0].id;
-
-							
-								var promise = new Promise((resolve,reject)=>{
-										connection.query("SELECT `name`,`text`,`updates` FROM `templates` WHERE `id_keygen`='"+id_keygen+"'",
-											(error,result,fields)=>{ 
-												var templates = JSON.stringify(result); //вытягиваем template
-													object_info.push({'templates':templates});		
-										});
-
-										connection.query("SELECT `text`,`date`,`updates` FROM `diaries` WHERE `id_keygen`='"+id_keygen+"'",
-											(error,result,fields)=>{ 
-												var diaries = JSON.stringify(result); //вытягиваем все заметки
-													object_info.push({'diaries':diaries});	
-															
-														});
-
-										connection.query("SELECT `note`,`updates` FROM `notes` WHERE `id_keygen`='"+id_keygen+"'",
-											(error,result,fields)=>{ 
-												var notes = JSON.stringify(result); //вытягиваем все заметки
-													object_info.push({'notes':notes});	
-															return resolve(object_info);
-														});
-										});
-
-								promise.then((value)=>{
-											io.to(socket.id).emit('global_info',{data:value})
-								});	
-						
-						}else{
-							console.log('Такого ключа не существует!');
-								io.to(socket.id).emit('global_info',{data:false}); //ключ не верный
-						}
-				});
-					
-					
-		});
-	
-});
-http.listen(3000, function(){
-  		console.log('listening on *:3000');
-});
+	ws.on('connection',(socket,req)=>{
+		var send_key = 'websocket-key: '+req.headers['sec-websocket-key'];
+		console.log('websocket-key: '+req.headers['sec-websocket-key']);
+			socket.send(send_key);
+	});
